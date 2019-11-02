d34eaafc3a7a37a7781f6462f33465fb310c089a
Jan Sękowski <j.a.sekowski@gmail.com>
2019-11-02 18:27:07 +0100

    praca domowa Ajax

--- Ajax/js/warsztaty-ajax-2.js
+++ Ajax/js/warsztaty-ajax-2.js
@@ -2,7 +2,7 @@
 
 $.getJSON("https://jsonplaceholder.typicode.com/users/1",
     function (data) {
-        // console.log(data);
+        console.log(data);
         $("#get-data-button").click(function () {
             $("#displayed-data").append("<li>" + data.id + ", " + data.username + ", " + data.website + "</li>");
         });
